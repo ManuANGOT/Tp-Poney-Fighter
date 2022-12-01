871be5318a772f281c6f863f70f619b31242b4ec
ManuAngot <manu.angot@hotmail.fr>
2022-12-01 16:48:57 +0100

    ajout poneyFighter

--- Classes/Poney.ts
+++ Classes/Poney.ts
@@ -1,36 +1,114 @@
-import Couleurs from "../Interfaces/Couleurs";
+import Couleurs from "../Couleurs";
 
 export default abstract class Poney {
-  private _nom: string;
-  private _dateDeNaissance: Date;
-  private _pvMax: number;
-  private _pv: number;
-  private _niveau:number;
-  private _couleur : Couleurs;
+  protected _nom!: string;
+  protected _dateNaissance!: Date;
+  protected _pvMax: number;
+  protected _pv: number;
+  protected _niveau: number;
+  protected _couleurs: Couleurs[];
 
-
-  constructor(nom :string, dateDeNaissance : Date, pvMax : number, pv: number, niveau?:number, couleur:Couleurs) {
-    this._nom= nom;
-    this._dateDeNaissance=dateDeNaissance;
-    this._pvMax=pvMax;
-    this._pv=pv;
-    this._niveau=niveau | 1;
-    this._couleur= couleur;
+  constructor(
+    nom: string,
+    dateNaissance: Date | string,
+    pv: number,
+    niveau?: number,
+    ...couleurs: Couleurs[]
+  ) {
+    this.nom = nom;
+    this.dateNaissance = dateNaissance;
+    this._pvMax = pv;
+    this._pv = pv;
+    this._niveau = niveau || 1;
+    this._couleurs = couleurs;
   }
-  public get nom(nouveauNom:string){
 
-    if (nouveauNom.trim().length>0){
-        this.nom = nouveauNom;
+  public get nom(): string {
+    return this._nom;
+  }
+
+  public set nom(nouveauNom: string) {
+    if (nouveauNom.trim().length > 0) {
+      this._nom = nouveauNom;
+    } else {
+      throw new Error("Le nom doit contenir au moins un caractère");
     }
-    else {
-        throw new error("Le nom doit contenir au moins un caractère");
-        
+  }
+
+  public get dateNaissance(): Date {
+    return this._dateNaissance;
+  }
+
+  public set dateNaissance(nouvelleDate: Date | string) {
+    if (nouvelleDate instanceof Date) {
+      this._dateNaissance = nouvelleDate;
+    } else {
+      this._dateNaissance = new Date(nouvelleDate);
     }
   }
 
-  public get dateDeNaissance():Date{
-    return this._dateDeNaissance;
+  public get pv(): number {
+    return this._pv;
   }
 
-  public set dateDeNaissance
+  public set pv(nouveauPv: number) {
+    if (nouveauPv <= this._pvMax) {
+      this._pv = nouveauPv;
+      console.log(`${this.nom} a maintenant ${this._pv} PV ! CHEH !`);
+    } else {
+      throw new Error(
+        "Le nombre de PV ne peut pas être supérieur à la valeur maximale"
+      );
+    }
+  }
+
+  get estVivant(): boolean {
+    if (this._pv > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public get niveau(): number {
+    return this._niveau;
+  }
+
+  public set niveau(nombreDeNiveaux: number) {
+    this._niveau += Math.abs(nombreDeNiveaux);
+  }
+
+  public get couleurs(): Couleurs[] {
+    return this._couleurs;
+  }
+
+  public set couleurs(couleurs: Couleurs[]) {
+    this._couleurs = couleurs;
+  }
+
+  /**
+   * Calcul le nombre de dégats infligés par le poney, en fonction de son niveau et du résultat d'un dés à 6 faces
+   * @returns Dégats infligés
+   */
+  public attaqueBasique() {
+    if (this.estVivant) {
+      const degats: number = Math.floor(Math.random() * 6 * this._niveau);
+      console.log(
+        `${this.nom} fait une attaque basique et fait ${degats} dégats ! Waouh !`
+      );
+      return degats;
+    } else {
+      console.log(`${this.nom} est mort, il ne peut pas attaquer !`);
+      return 0;
+    }
+  }
+
+  /**
+   * Se rend des PV, et rend la moitié à l'adversaire
+   * @returns Soin apporté
+   */
+  public distribuerDeLAmour(): number {
+    this.pv += this._niveau * 2;
+    return this._niveau;
+  }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+Démonstration improvisée sur un combat de poneys en arène qui met pratique les concepts suivants :
+
+Héritage,
+Agrégation,
+Abstraction,
+Polymorphisme,
+Encapsulation
